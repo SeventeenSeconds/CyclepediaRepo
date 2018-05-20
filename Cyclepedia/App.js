aede9a790cbbd46235b1e273815191682edf688e
Maddy Richard <MRichard@student.neumont.edu>
2018-05-19 18:54:44 -0600

    Got fonts, colors, and icons working

--- Cyclepedia/App.js
+++ Cyclepedia/App.js
@@ -1,24 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import { MapTest } from './MapTest';
-//import { Map } from './Map';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Geolocation } from './Geolocation';
+import { AppLoading, Font } from 'expo';
+import { colorStyles } from './constants/colors';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Raleway': require('./assets/fonts/Raleway/Raleway-Regular.ttf'),
+        'Work_Sans': require('./assets/fonts/Work_Sans/WorkSans-Regular.ttf'),
+        'FontAwesome': require('./assets/fonts/fontawesome.ttf')
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) {
+      return <Expo.AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-      <Geolocation />
+    <View style={styles.container}>
+      
+      <Text style={styles.headingFont}>
+        I am a Raleway heading
+      </Text>
+      <Text style={styles.bodyFont}>
+        I am Work Sans body text
+      </Text>
+
+      <View style={styles.grid}>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('./assets/images/fabicycle.png')}
+            style={styles.icon}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('./assets/images/fachart.png')}
+            style={styles.icon}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('./assets/images/faexclamation.png')}
+            style={styles.icon}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Image
+            source={require('./assets/images/fahome.png')}
+            style={styles.icon}
+          />
+        </View>
       </View>
-    );
+
+    </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headingFont: {
+    fontFamily: 'Raleway',
+    fontSize: 40,
+    color: colorStyles.blue
+  },
+  bodyFont: {
+    fontFamily: 'Work_Sans',
+    fontSize: 20
+  },
+  grid: {
+    marginTop: 10,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  iconContainer: {
+    flex: 1,
+    height: 40,
+    width: 40
+  },
+  icon: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: undefined,
+    height: undefined,
+    resizeMode: 'contain'
+  }
 });
--- Cyclepedia/constants/colors.js
+++ Cyclepedia/constants/colors.js
@@ -1,13 +1,14 @@
-const black = "#000";
-const white = "#F2F2F2";
-const yellow = "#F9DC5C";
-const green = "#E5F993";
-const blue = "#69A197";
+const black = '#000';
+const white = '#F2F2F2';
+const yellow = '#F9DC5C';
+const green = '#E5F993';
+const blue = '#69A197';
 
-export default {
-  black,
-  white,
-  yellow,
-  green,
-  blue
-}
+export const colorStyles = {
+  black: black,
+  white: white,
+  yellow: yellow,
+  green: green,
+  blue: blue
+};
+  
