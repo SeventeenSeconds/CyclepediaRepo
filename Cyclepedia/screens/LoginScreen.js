cbee8d66dcc6f8909f1b417c11ebc4c81065cb25
Emily Parr <eparr@student.neumont.edu>
2018-05-28 21:37:17 -0600

    Login Password Decrypt working!

--- Cyclepedia/screens/LoginScreen.js
+++ Cyclepedia/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, AsyncStorage, Button, Text } from 'react-native';
+import {View, ScrollView, StyleSheet, AsyncStorage, Button, Text} from 'react-native';
 import colorStyles from '../constants/colors';
 import {AppLoading} from 'expo';
 
@@ -40,22 +40,19 @@ function comparePass(currentUser) {
 }
 
 encrypt = passwrd => {
-    console.log("in encrypt");
-
-    var unencrypt = 'bacon';
 
     var ciphertext = crypto.AES.encrypt(passwrd, 'secret key 123');
     console.log("encrypted text", ciphertext.toString());
 
     var bytes = crypto.AES.decrypt(ciphertext.toString(), 'secret key 123');
-    var plaintext = bytes.toString(crypto.enc.Utf8);
-    console.log("decrypted text", plaintext);
+    // var plaintext = bytes.toString(crypto.enc.Utf8);
+    console.log("decrypted text " + bytes);
 
     return ciphertext.toString();
 }
 
 decrypt = passwrd => {
-    var bytes = crypto.AES.decrypt(ciphertext.toString(), 'secret key 123');
+    var bytes = crypto.AES.decrypt(passwrd, 'secret key 123');
     var plaintext = bytes.toString(crypto.enc.Utf8);
     return plaintext;
 }
@@ -87,34 +84,45 @@ export default class LoginScreen extends React.Component {
 
     state = {
         userMessage: "",
+        userLoggedIn: false,
     }
 
-    async getUser(userData){
+    async getUser(userData) {
         try {
             var u = null;
             const value = await AsyncStorage.getItem(userData.email.toString()).then((keyValue) => {
-                u = keyValue, console.log("user: " + u)}, (error) => {
+                u = keyValue}, (error) => {
                 console.log(error);
                 this.setState({userMessage: "Error retrieving user data, please try again."});
             });
+
             if (u != null) {
                 // user exists, hash password in form, compare the returned users hashed password
-                const userPass = decrypt(u.password);
-                console.log("User Password: " + u.password + " Hashed Password: " + userPass);
-                if(userPass == u.password){
-                    //TODO: user logged in, navigate to bottom tab
-                    console.log("Successfully logged in!");
-                } else {
-                    //TODO: wrong password, try again
-                    console.log("wrong password, try again");
-                }
+                console.log("validating password")
+                this.validatePassword(u, userData.password);
             } else {
                 //TODO: User doesn't exist, please register
                 console.log("User doesn't exists, please register");
             }
         } catch (error) {
-            console.log("Internal Error in catch");
+            console.log("Error retrieving user data, please try again.");
+            console.log(error);
             //TODO: Set message
+        }
+    }
+
+    validatePassword(userPromise, userPassword) {
+        userPromise = JSON.parse(userPromise);
+        const decryptedPassword = decrypt(userPromise.password);
+        console.log("Decrypted " + decryptedPassword + " userpass " + userPassword);
+        if (decryptedPassword == userPassword ) {
+            //TODO: user logged in, navigate to bottom tab
+            console.log("Successfully logged in!");
+            // this.setState({userCreated: true});
+        } else {
+            //TODO: wrong password, try again
+            console.log("wrong password, try again");
+
         }
     }
 
@@ -135,7 +143,9 @@ export default class LoginScreen extends React.Component {
             const user = this.getUser(userData);
 
             // console.log(user);
-            // this.props.navigation.navigate('Bottom');
+            if (this.state.userLoggedIn) {
+                this.props.navigation.navigate('Bottom');
+            }
         }
 
     }
