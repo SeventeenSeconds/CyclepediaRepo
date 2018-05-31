ba5d578063bc0efe494aca0d377cd225e838d6a4
Maddy Richard <MRichard@student.neumont.edu>
2018-05-31 17:04:39 -0600

    Added geolocation tracking/distance calculation to MapTest.js

--- Cyclepedia/MapTest.js
+++ Cyclepedia/MapTest.js
@@ -2,15 +2,97 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Constants, Location, Permissions, MapView } from 'expo';
 
+const GEOLOCATION_OPTIONS = {
+  enableHighAccuracy: true,
+  timeout: 20000,
+  maximumAge: 1000
+};
+
+var isPaused = false;
+var isEndRide = false;
+
+let startCoords = {
+  latitude: null,
+  longitude: null,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.05,
+}
+
+let pauseCoords = {
+  latitude: null,
+  longitude: null,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.05,
+}
+
+let endCoords = {
+  latitude: null,
+  longitude: null,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.05,
+}
+
 export default class App extends Component {
   state = {
     locationResult: null,
     longitude: null,
     latitude: null,
+    location: {
+      coords: {
+        latitude: 0,
+        longitude: 0
+      }
+    },
   };
 
   componentDidMount() {
     this._getLocationAsync();
+  }
+
+  componentWillMount() {
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+  }
+
+  locationChanged = (location) => {
+    //these will be set on button click
+    if (isPaused == true) {
+      pauseCoords.latitude = location.coords.latitude;
+      pauseCoords.longitude = location.coords.longitude;
+    }
+    if (isEndRide == true) {
+      endCoords.latitude = location.coords.latitude;
+      endCoords.longitude = location.coords.longitude;
+    }
+
+    startCoords.latitude = location.coords.latitude;
+    startCoords.longitude = location.coords.latitude;
+
+    region = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.05,
+      },
+      this.setState({
+        location,
+        region
+      })
+  }
+
+  calculateDistance = (lat1, lat2, lon1, lon2) => {
+    var R = 6371e3;
+    var lat1Rad = lat1.toRadians();
+    var lat2Rad = lat2.toRadians();
+    var distLatRad = (lat2 - lat1).toRadians();
+    var distLonRad = (lon2 - lon1).toRadians();
+
+    var a = Math.sin((lat1Rad / 2)) * (Math.sin(distLatRad / 2)) +
+      Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+      Math.sin(distLatRad / 2) * Math.sin(distLonRad / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    var d = R * c;
+    console.log("distance", d);
   }
 
   _getLocationAsync = async () => {
@@ -71,7 +153,9 @@ export default class App extends Component {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421
             }}
+        region={this.state.region}
         onRegionChange={this._handleMapRegionChange}
+        showsUserLocation={true}
         >
         <MapView.Marker 
           coordinate={{
