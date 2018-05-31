8fb88fabf70dae2667e909db1e8d084a2b461502
Maddy Richard <MRichard@student.neumont.edu>
2018-05-31 16:00:19 -0600

    MapTest can get current location and set it as a marker on the map view

--- Cyclepedia/MapTest.js
+++ Cyclepedia/MapTest.js
@@ -4,50 +4,79 @@ import { Constants, Location, Permissions, MapView } from 'expo';
 
 export default class App extends Component {
   state = {
-    locationResult: null
+    locationResult: null,
+    longitude: null,
+    latitude: null,
   };
 
   componentDidMount() {
     this._getLocationAsync();
   }
-  
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Permission to access location was denied',
-     });
-   }
-   
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Permission to access location was denied',
-     });
-   }
 
-    let locationObj = null;
-    let locationStateArray = [];
-    let location = await Location.getCurrentPositionAsync({}).then(function() {
-     locationObj = JSON.parse(location);
-    this.setState({ locationResult: "hello" });
-   }.bind(this));
+  _getLocationAsync = async () => {
+    let {
+      status
+    } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+      });
+    }
+
+    let locationResult = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+
+    this.setState({
+      locationResult
+    });
+    let latitude = locationResult.coords.latitude;
+    this.setState({
+      latitude
+    });
+    let longitude = locationResult.coords.longitude;
+    this.setState({
+      longitude
+    });
   }
 
   render() {
+    let text = JSON.stringify(this.state.locationResult);
+    let latText = JSON.stringify(this.state.latitude);
+    let longText = JSON.stringify(this.state.longitude);
+    let latNum = parseFloat(latText);
+    let longNum = parseFloat(longText);
     return (
       <View style={styles.container}>
         <Text>
-          Location: {this.state.locationResult}
+          Location: {text}
+        </Text>
+        <Text>
+          Latitude: {latText}
+        </Text>
+        <Text>
+          LatNum: {latNum}
+        </Text>
+        <Text>
+          Longitude: {longText}
+        </Text>
+        <Text>
+          LongNum: {longNum}
         </Text>
         <MapView
         style={{ alignSelf: 'stretch', height: 200 }}
-        region={this.state.mapRegion}
+        initialRegion={{
+                latitude: 40.760779,
+                longitude: -111.891047,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}
         onRegionChange={this._handleMapRegionChange}
         >
         <MapView.Marker 
           coordinate={{
-            latitude: 39.7392,
-            longitude: -104.9903,
+            latitude: latNum,
+            longitude: longNum,
           }}
           title={'Current Location'}
           description={'I am here'}
